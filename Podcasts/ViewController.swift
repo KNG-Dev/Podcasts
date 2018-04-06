49d364ede17461d71a74e75861a8ef29348bdf1b
Kenny Ho <ken.ho1996@hotmail.com>
2018-04-06 07:19:53 -0400

    Initial Commit

---
+++ Podcasts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Podcasts
+//
+//  Created by Kenny Ho on 4/6/18.
+//  Copyright © 2018 Kenny Ho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
